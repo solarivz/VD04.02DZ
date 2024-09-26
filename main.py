45a4c3d0bb2308fa874e9d12c98dd582193d6d31
Ivan Zyulin <solar.ivz@gmail.com>
2024-09-27 00:38:12 +1200

    upd

--- main.py
+++ main.py
@@ -2,21 +2,20 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
-@app.route("/")
+# Маршрут для главной страницы
+@app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template('index.html')
 
-
-@app.route("/blog/")
+# Маршрут для страницы блога
+@app.route('/blog')
 def blog():
-    return render_template("blog.html")
+    return render_template('blog.html')
 
-
-@app.route("/contacts/")
+# Маршрут для страницы контактов
+@app.route('/contacts')
 def contacts():
-    return render_template("contacts.html")
-
+    return render_template('contacts.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
