1f29f179ae716561f37ae8e15a1008a80eb4b1fb
Ivan Zyulin <solar.ivz@gmail.com>
2024-09-26 22:50:59 +1200

    upd main add content

--- main.py
+++ main.py
@@ -4,14 +4,19 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def films():
+def index():
     return render_template("index.html")
 
 
-@app.route("/index2/")
-def person():
-    return render_template("index2.html")
+@app.route("/blog")
+def blog():
+    return render_template("blog.html")
 
 
-if __name__ == "__main__":
-    app.run()
+@app.route("/contacts")
+def contacts():
+    return render_template("contacts.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
